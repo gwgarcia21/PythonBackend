53de06c03c89a61d2db60aad8a59d5e1c82111fd
Guilherme <guilherme@duelaser.com>
2025-10-11 23:19:38 -0300

    4

---
+++ Module2/Lesson1/Exercise4/api.py
@@ -0,0 +1,38 @@
+"""Application Factory Exercise: Modify the application factory example to use 
+pydantic-settings for configuration management. Create separate .env files for 
+development and production environments and load the appropriate settings based 
+on the environment."""
+
+import importlib
+from fastapi import FastAPI
+from .routers import products, users
+from .middlewares.logging import LoggingMiddleware
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    app_name: str = "Awesome API"
+    debug: bool = False
+    database_url: str = "sqlite:///./test.db"
+
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore") # Load from .env file and ignore extra variables
+
+def create_app(settings_module="config.development"):
+    app = FastAPI()
+    
+    package = __package__ or __name__.rpartition('.')[0]
+    if package and not settings_module.startswith(package) and not settings_module.startswith("."):
+        import_name = f"{package}.{settings_module}"
+    else:
+        import_name = settings_module
+    settings = importlib.import_module(import_name)
+    app.state.settings = settings
+
+    app.add_middleware(LoggingMiddleware)
+    app.include_router(users.router)
+    app.include_router(products.router)
+
+    return app
+
+app = create_app(settings_module="config.production")
+
+# Rodar com: uv run fastapi dev api.py
